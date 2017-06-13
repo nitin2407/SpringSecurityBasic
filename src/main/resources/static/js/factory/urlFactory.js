294a99118cec838f13971fd9c549895716a80a1c
Nitin <nitinpurohit@gmail.com>
2017-06-13 15:43:23 -0400

    code cleanup

--- src/main/resources/static/js/factory/urlFactory.js
+++ src/main/resources/static/js/factory/urlFactory.js
@@ -5,6 +5,12 @@ app.factory('urlFactory', function($window){
 	urlFactory.home = function(){ 
 		return "http://" + $window.location.host + "/index.html"; 
 	};
+	urlFactory.adminhome = function(){ 
+		return "http://" + $window.location.host + "/html/admin-home.html"; 
+	};
+	urlFactory.userhome = function(){ 
+		return "http://" + $window.location.host + "/index.html"; 
+	};
 
 	return urlFactory;
 	
--- target/classes/static/js/controllers/loginController.js
+++ target/classes/static/js/controllers/loginController.js
@@ -1,35 +1,4 @@
-app.controller('loginCtrl', function ($scope, $http, $timeout, $window, loginService, urlFactory) {
-
-
-	$scope.fbauth = function () {
-		loginService.fblogin().then(
-			function (data, status) {
-				console.log(data);
-			},
-			function (data, status) {
-				swal({
-					title: 'error',
-					type: 'warning',
-					showCloseButton: true
-				});
-			}
-		)
-	}
-
-	$scope.googleauth = function () {
-		loginService.googlelogin().then(
-			function (data, status) {
-				console.log(data);
-			},
-			function (data, status) {
-				swal({
-					title: 'error',
-					type: 'warning',
-					showCloseButton: true
-				});
-			}
-		)
-	}
+app.controller('loginCtrl', function ($rootScope,$scope, $http, $timeout, $window, loginService, urlFactory) {
 
 	$scope.submitForm = function (isValid) {
 
@@ -48,44 +17,29 @@ app.controller('loginCtrl', function ($scope, $http, $timeout, $window, loginSer
 			loginService.login(headers).then(
 				function (data, status) {
 					console.log(data);
-					$scope.userDetails = data.data;
-					//console.log($scope.userDetails);
-					if ($scope.userDetails.message) {
-
-						//alert(data.data.message[0]);
-						swal({
-							title: data.data.message[0],
-							type: 'warning',
-							showCloseButton: true
-						});
+					$rootScope.currentUser = data.data;
+					$rootScope.isAuthenticated = true;
+					if($rootScope.currentUser.userRole.role_id == 2){
+						$timeout(function () {
+	                    	$window.location.href = urlFactory.adminhome();
+	                	}, 2000)
+					}
+					else{
+						$timeout(function () {
+	                    	$window.location.href = urlFactory.home();
+	                	}, 2000)
 					}
 				},
 				function (error) {
 					console.log(error);
-
-					//if (status == 401) {
-
-					//alert(data.data[0].message);
 					swal({
 						title: error.data.error,
 						type: 'error',
 						showCloseButton: true
 					});
 
-
-					/*swal({
-	                    title: 'not able to login',
-	                    type: 'warning',
-	                    showCloseButton: true	
-	                });*/
-					$timeout(function () {
-						//$window.location.href = urlFactory.home();
-					}, 2000)
 				}
-			).catch(function (data) {
-				console.log(data)
-			});
-		};
+			)};
 	};
 
 
